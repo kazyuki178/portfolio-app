902917f3ecef38d07889ecca6a8cbc6f6b745329
kazuki178 <yahashi.kazuki0521@icloud.com>
2025-08-23 18:03:30 +0900

    appに非同期処理の宣言を追加

--- frontend/my-react-app/src/app/page.tsx
+++ frontend/my-react-app/src/app/page.tsx
@@ -1,24 +1,17 @@
-
-
 import Profile from '../components/Profile';
 import RecentActivity from '../components/RecentActivity';
 
-
 export const metadata = {
   title: "kazuki’s Portfolio Top | ITエンジニア 矢橋和樹 |",
-  description: "kazuki’s Portfolio Top。ITエンジニア矢橋和樹（Kazuki Yahashi）の経歴、スキル、Web制作・インフラ構築の実績を掲載したポートフォリオサイトです。",
+  description: "kazuki’s Portfolio Top。ITエンジニア矢橋和樹（Kazuki Yahashi）",
 };
 
-const App = () => {
+// async function にする
+export default async function App() {
   return (
     <>
-
       <Profile />
-
-      <RecentActivity />
+      <RecentActivity /> 
     </>
   );
-
-};
-
-export default App;
+}
