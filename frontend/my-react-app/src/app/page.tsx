350255b52ed735decffc9ab72f05d0d7d0c4cb14
kazuki178 <yahashi.kazuki0521@icloud.com>
2025-08-23 18:09:59 +0900

    修正

--- frontend/my-react-app/src/app/page.tsx
+++ frontend/my-react-app/src/app/page.tsx
@@ -1,13 +1,13 @@
 import Profile from '../components/Profile';
 import RecentActivity from '../components/RecentActivity';
-
+import { ReactElement } from 'react';
 export const metadata = {
   title: "kazuki’s Portfolio Top | ITエンジニア 矢橋和樹 |",
   description: "kazuki’s Portfolio Top。ITエンジニア矢橋和樹（Kazuki Yahashi）",
 };
 
 
-export default async function App() {
+export default async function App(): Promise<ReactElement>  {
   return (
     <>
       <Profile />
