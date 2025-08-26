be1326fe4110905e1416b0368c45be0119e9ad67
kazuki178 <yahashi.kazuki0521@icloud.com>
2025-08-26 18:09:48 +0900

    リファクタリング

--- frontend/my-react-app/src/app/page.tsx
+++ frontend/my-react-app/src/app/page.tsx
@@ -1,6 +1,6 @@
 import Profile from '../components/Profile';
 import RecentActivity from '../components/RecentActivity';
-import { ReactElement } from 'react';
+
 export const metadata = {
   title: "kazuki’s Portfolio Top | ITエンジニア 矢橋和樹 |",
   description: "kazuki’s Portfolio Top。ITエンジニア矢橋和樹（Kazuki Yahashi）",
